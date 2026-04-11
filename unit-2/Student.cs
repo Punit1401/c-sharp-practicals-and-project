e55637a0ff7411fb25acada343c967a34a40a17e
Punit Nakum <125398856+Punit1401@users.noreply.github.com>
2026-04-11 12:08:15 +0530

    Create Student.cs

---
+++ unit-2/Student.cs
@@ -0,0 +1,57 @@
+// Question:
+// Define a class Student, which contains the following information
+// about students: full name, course, subject, university, e-mail and
+// phone_number. Add two methods to collect and display information.
+
+using System;
+
+namespace lab_work_1
+{
+    class Student
+    {
+        string fullName, course, subject, university, email, phoneNumber;
+
+        // Method to collect data
+        public void GetData()
+        {
+            Console.Write("Enter Full Name: ");
+            fullName = Console.ReadLine();
+
+            Console.Write("Enter Course: ");
+            course = Console.ReadLine();
+
+            Console.Write("Enter Subject: ");
+            subject = Console.ReadLine();
+
+            Console.Write("Enter University: ");
+            university = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            email = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            phoneNumber = Console.ReadLine();
+        }
+
+        // Method to display data
+        public void DisplayData()
+        {
+            Console.WriteLine("\n--- Student Details ---");
+            Console.WriteLine("Name: " + fullName);
+            Console.WriteLine("Course: " + course);
+            Console.WriteLine("Subject: " + subject);
+            Console.WriteLine("University: " + university);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Phone Number: " + phoneNumber);
+        }
+
+        static void Main()
+        {
+            Student s = new Student();
+            s.GetData();
+            s.DisplayData();
+
+            Console.ReadLine();
+        }
+    }
+}
